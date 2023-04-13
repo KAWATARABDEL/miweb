9390d33bc3cb2900f8951855aeba457fffde9904
Abdelouahd El mezouari <soyabdel1@gmail.com>
2023-04-13 02:34:30 +0200

    mostrando status corto

--- archivo1.py
+++ archivo1.py
@@ -3,3 +3,4 @@
 print("mi primera comprovacion de utilizar vs code con git")
 
 # si modificas el codigo debes volver a aniadir con git add archivo1.py para que storage se de cuenta de cada ejecucion que haces.
+print("abdel")
